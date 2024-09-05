af07e488c37738213e5587d715a1ff2fd30a0c62
Artut <ArtSoull@yandex.ru>
2024-09-05 20:22:00 +0500

    from job

--- app/src/main/java/com/example/weatherapi/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapi/fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package com.example.weatherapi.fragments
 
 import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
@@ -20,6 +22,10 @@ import com.example.weatherapi.MainViewModel
 import com.example.weatherapi.adapters.VpAdapter
 import com.example.weatherapi.adapters.WeatherModel
 import com.example.weatherapi.databinding.FragmentMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
 import com.squareup.picasso.Picasso
 import org.json.JSONArray
@@ -28,6 +34,7 @@ import org.json.JSONObject
 const val API_KEY = "0aeb62cf9bfd4f3aacd175537241308"
 
 class MainFragment : Fragment() {
+    private lateinit var fLocationClient: FusedLocationProviderClient
     private val fList = listOf(
         HoursFragment.newInstance(),
         DaysFragment.newInstance()
@@ -53,10 +60,30 @@ class MainFragment : Fragment() {
         checkPermission()
         init()
         updateCurrentCard()
-        requestWeatherData("Ufa")
+        getLocation()
+    }
+
+    private fun getLocation() {
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fLocationClient
+            .getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, ct.token)
+            .addOnCompleteListener {
+                requestWeatherData("${it.result.latitude}, ${it.result.longitude}")
+            }
     }
 
     private fun init() = with(binding) {
+        fLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         val adapter = VpAdapter(activity as FragmentActivity, fList)
         vp.adapter = adapter
         TabLayoutMediator(tabLayout2, vp) { tab, pos ->
@@ -64,14 +91,14 @@ class MainFragment : Fragment() {
         }.attach()
     }
 
-    private fun updateCurrentCard() = with(binding){
+    private fun updateCurrentCard() = with(binding) {
         model.liveDataCurrent.observe(viewLifecycleOwner) { it ->
             val maxMinTemp = "${it.maxTemp}°C/${it.minTemp}°C"
             tvData.text = it.time
             tvCity.text = it.city
-            tvCurrentTemp.text = it.currentTemp
+            tvCurrentTemp.text = it.currentTemp.ifEmpty { maxMinTemp }
             tvCondition.text = it.condition
-            tvMaxMin.text = maxMinTemp
+            tvMaxMin.text = if (it.currentTemp.isEmpty()) "" else maxMinTemp
             Picasso.get().load("https:" + it.imageUrl).into(imWeather)
         }
     }
@@ -118,16 +145,16 @@ class MainFragment : Fragment() {
         val list = ArrayList<WeatherModel>()
         val daysArray: JSONArray = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
 
-        val city =  mainObject.getJSONObject("location").getString("name")
+        val city = mainObject.getJSONObject("location").getString("name")
         for (i in 0 until daysArray.length()) {
             val day = daysArray[i] as JSONObject
-            val item = WeatherModel (
+            val item = WeatherModel(
                 city = city,
                 time = day.getString("date"),
                 condition = day.getJSONObject("day").getJSONObject("condition").getString("text"),
                 currentTemp = "",
-                maxTemp = day.getJSONObject("day").getString("maxtemp_c"),
-                minTemp = day.getJSONObject("day").getString("mintemp_c"),
+                maxTemp = day.getJSONObject("day").getString("maxtemp_c").toFloat().toInt().toString(),
+                minTemp = day.getJSONObject("day").getString("mintemp_c").toFloat().toInt().toString(),
                 imageUrl = day.getJSONObject("day").getJSONObject("condition").getString("icon"),
                 hours = day.getJSONArray("hour").toString()
             )
